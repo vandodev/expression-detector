16f740af695f3c6880da7014739d2e579ce93e05
vandodev <vandoaparecido@hotmail.com>
2023-11-25 23:11:19 -0300

    :smiley: :neutral_face: :wink: Detectando face

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,11 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from "react";
 import {  View } from 'react-native';
 import { styles } from './styles';
+import {Camera, CameraType, FaceDetectionResult } from 'expo-camera';
+import * as FaceDetector from "expo-face-detector";
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+  } from "react-native-reanimated";
 
 export function Home() {
+    const [faceDetected, setFaceDetected] = useState(false);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    
+    useEffect(() =>{
+        requestPermission();
+    },[])
+
+    if(!requestPermission){
+        return;
+    }
+
+    const faceValues = useSharedValue({
+        width: 0,
+        height: 0,
+        x: 0,
+        y: 0,
+      });
+
+      const animatedStyle = useAnimatedStyle(() => ({
+        position: "absolute",
+        zIndex: 1,
+        width: faceValues.value.width,
+        height: faceValues.value.height,
+        transform: [
+          { translateX: faceValues.value.x },
+          { translateY: faceValues.value.y },
+        ],
+        borderColor:'blue',
+        borderWidth:10,
+      }));
+
+    function handleFacesDetected({ faces }: FaceDetectionResult) {
+        const face = faces[0] as any;
+    
+        if (face) {
+          const { size, origin } = face.bounds;
+    
+          faceValues.value = {
+            height: size.height,
+            width: size.width,
+            x: origin.x,
+            y: origin.y,
+          };
+    
+          setFaceDetected(true);
+             
+        } else {
+          setFaceDetected(false);
+        }
+      }
+    
+
   return (
     <View style={styles.container}>
-        
+         {faceDetected && <Animated.View style={animatedStyle}/>}
+        <Camera 
+            style={styles.camera}
+            type={CameraType.front}
+            onFacesDetected={handleFacesDetected}
+            faceDetectorSettings={{
+                mode: FaceDetector.FaceDetectorMode.fast,
+                detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+                runClassifications: FaceDetector.FaceDetectorClassifications.all,
+                minDetectionInterval: 100,
+                tracking: true,
+              }}
+         />
     </View>
   );
 }
--- src/home/styles.ts
+++ src/home/styles.ts
@@ -2,6 +2,9 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-      flex: 1,
+      flexGrow:1
+    },
+    camera: { 
+        flex:1
     },
   });
