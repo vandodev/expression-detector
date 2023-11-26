cf9b1ddde2fcfebe7298b2f1ca5ae3336b81e43b
vandodev <vandoaparecido@hotmail.com>
2023-11-25 21:45:53 -0300

    :smiley: :neutral_face: :wink: Estrutura e dependências

---
+++ src/home/index.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {  View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+        
+    </View>
+  );
+}
+
+
